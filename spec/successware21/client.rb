0acf04de455c1f825724243ab9ffe4ae6c774895
Osman Afridi <osmanafridi@Osmans-MacBook-Pro.local>
2016-03-25 10:18:06 -0700

    Fix specs

--- spec/successware21/client.rb
+++ spec/successware21/client.rb
@@ -3,8 +3,9 @@ require 'spec_helper'
 RSpec.describe Successware21::Client do
 
   it 'should connect using the configured endpoint and api version' do
-    client     = Successware21::Client.new
-    endpoint   = URI.parse("#{client.endpoint}#{client.api_version}/")
+    valid_params = { agent_name: 'test_agent', user_name: 'test_user', user_password: 'test_user_pw', agent_password: 'test_agent_pw', master_id: 123 }
+    client     = Successware21::Client.new(valid_params)
+    endpoint   = URI.parse("#{client.endpoint}/")
     connection = client.send(:connection).build_url(nil).to_s
     expect(connection).to eq(endpoint.to_s)
   end
